4e08ac95455ce65c48e2e660b1cd1fe2840c0400
pelitaf <pelita.felicitas@gmail.com>
2024-11-18 21:01:10 -1000

    changed back restaurant card

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,61 +1,36 @@
+import { Restaurant } from '@/lib/validationSchemas';
 import { Card, ListGroup, Button } from 'react-bootstrap/';
 import { Heart } from 'react-bootstrap-icons';
 import Link from 'next/link';
 
-interface RestaurantCardProps {
-  restaurant: {
-    id: number;
-    name: string;
-    website: string | null;
-    phone: string | null;
-    menuLink: string | null;
-    onlineOrderLink: string | null;
-  };
-}
-
-const RestaurantCard = ({ restaurant }: RestaurantCardProps) => (
+const RestaurantCard = ({ restaurant }: { restaurant: Restaurant }) => (
   <Card className="h-100 mb-3">
     <Card.Header>
-      <Card.Title>{restaurant.name}</Card.Title>
+      <Card.Title>
+        {restaurant.name}
+      </Card.Title>
       <Card.Subtitle>
-        {restaurant.website ? (
-          <Link href={restaurant.website} target="_blank" rel="noopener noreferrer">
-            {restaurant.website}
-          </Link>
-        ) : (
-          'Website not available'
-        )}
+        <Link href={restaurant.website} target="_blank" rel="noopener noreferrer">
+          {restaurant.website}
+        </Link>
       </Card.Subtitle>
     </Card.Header>
     <Card.Body>
       <ListGroup variant="flush">
         <ListGroup.Item>
-          <strong>Phone:</strong>
-          {' '}
+          Phone:
           {restaurant.phone}
         </ListGroup.Item>
         <ListGroup.Item>
-          {restaurant.menuLink ? (
-            <Link href={restaurant.menuLink} target="_blank" rel="noopener noreferrer">
-              Menu
-            </Link>
-          ) : (
-            'Menu not available'
-          )}
+          <Link href={restaurant.menuLink} target="_blank" rel="noopener noreferrer">Menu</Link>
         </ListGroup.Item>
         <ListGroup.Item>
-          {restaurant.onlineOrderLink ? (
-            <Link href={restaurant.onlineOrderLink} target="_blank" rel="noopener noreferrer">
-              Order Online
-            </Link>
-          ) : (
-            'Online ordering not available'
-          )}
+          <Link href={restaurant.onlineOrderLink} target="_blank" rel="noopener noreferrer">Order Online</Link>
         </ListGroup.Item>
       </ListGroup>
     </Card.Body>
     <Card.Footer>
-      <Button variant="link">
+      <Button variant="secondary">
         <Heart />
       </Button>
     </Card.Footer>
