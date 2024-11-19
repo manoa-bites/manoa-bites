64ac6d29548d0d4c26b658a26c0515c0dfca0c43
pelitaf <pelita.felicitas@gmail.com>
2024-11-19 00:48:25 -1000

    change back. failed to build

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,41 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React from 'react';
+import { Row, Col, Container } from 'react-bootstrap';
 
-const center = {
-  lat: 25.0326996781907,
-  lng: -77.47713270775661
-};
-
-const App = () => {
-  const [mapLoaded, setMapLoaded] = useState(false);
-
-  useEffect(() => {
-    // Simulate loading state for Google Maps API
-    setMapLoaded(true); 
-  }, []);
-
-  if (!mapLoaded) {
-    return <div>Loading...</div>; // Show loading indicator while the map is loading
-  }
-
+export default function App() {
   return (
     <Container>
       <Row className="w-100">
         <Col lg={6} className="my-4">
-          <LoadScript googleMapsApiKey="AIzaSyAIKosgU22MTHQkgMQ4qfV0mI71UFhtlbk">
-            <GoogleMap
-              mapContainerStyle={{ height: '400px', width: '100%' }}
-              center={center}
-              zoom={11}
-            >
-              <Marker position={center} />
-            </GoogleMap>
-          </LoadScript>
+          <iframe
+            // eslint-disable-next-line max-len
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115681.29592731265!2d-77.47713270775661!3d25.0326996781907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x892f7c99b981dbc9%3A0x2aef01d3485e50d2!2sNassau%2C%20Bahamy!5e0!3m2!1spl!2spl!4v1624445118063!5m2!1spl!2spl"
+            className="w-100"
+            height="400"
+            loading="lazy"
+            title="Google Map"
+          />
         </Col>
       </Row>
     </Container>
   );
-};
-
-export default App;
+}
