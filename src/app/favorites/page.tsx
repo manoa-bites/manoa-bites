1baf38c914241f22b38edf52ae547c06130562e8
pelitaf <pelita.felicitas@gmail.com>
2024-11-19 00:05:07 -1000

    removed fluid for formatting

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -44,7 +44,7 @@ const FavoritesPage = async () => {
 
   return (
     <main>
-      <Container id="list" fluid className="py-3">
+      <Container id="list" className="py-3">
         <h1 className="text-center">Your Favorite Restaurants</h1>
         <Row>
           <Col>
