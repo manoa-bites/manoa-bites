f43ca35e3fc8a343e7d461ae28b14129ab853571
pelitaf <pelita.felicitas@gmail.com>
2024-11-19 23:39:04 -1000

    trying to center map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -2,7 +2,7 @@ import App from '@/components/MapComponent';
 import { Container } from 'react-bootstrap';
 
 const MapPage = () => (
-  <Container>
+  <Container className="justify-content-center">
     <h1 className="text-center p-4">Need Directions?</h1>
     <App />
   </Container>
