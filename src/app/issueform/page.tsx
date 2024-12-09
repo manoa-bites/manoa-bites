dad149fa841d1e55118aa1f34d18d672635b5b47
pelitaf <pelita.felicitas@gmail.com>
2024-12-09 10:54:14 -1000

    fixed report problem
    fixed it again so that anyone can report problem readded link on footer link.

--- src/app/issueform/page.tsx
+++ src/app/issueform/page.tsx
@@ -1,21 +1,9 @@
-import { getServerSession } from 'next-auth';
-import authOptions from '@/lib/authOptions';
-import { loggedInProtectedPage } from '@/lib/page-protection';
 import IssueForm from '@/components/IssueForm';
 
-const ReportAProblem = async () => {
-  // Protect the page, only logged in users can access it.
-  const session = await getServerSession(authOptions);
-  loggedInProtectedPage(
-    session as {
-      user: { email: string; id: string; randomKey: string };
-    } | null,
-  );
-  return (
-    <main>
-      <IssueForm />
-    </main>
-  );
-};
+const ReportAProblem = async () => (
+  <main>
+    <IssueForm />
+  </main>
+);
 
 export default ReportAProblem;
