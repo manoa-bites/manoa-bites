dd34bae975fca250563b0e34f565ac8d1cf64705
pelitaf <pelita.felicitas@gmail.com>
2024-12-10 09:56:22 -1000

    individual restaurant pages designed

--- src/app/restaurant/[restaurantId]/page.tsx
+++ src/app/restaurant/[restaurantId]/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma';
-import { Container } from 'react-bootstrap';
+import { Container, Image, Col, Row } from 'react-bootstrap';
 
 interface RestaurantPageProps {
   params: { restaurantId: string };
@@ -36,10 +36,55 @@ const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   // Return restaurant page if restaurant found
   // Implement restaurant page design here.
   return (
-    <Container className="justify-content-center p-4 d-flex">
-      {restaurant?.name}
-      {restaurant?.locationId}
-      {locationName}
+    <Container className="py-3">
+      <Image
+        className="py-3 mx-auto d-block"
+        src={`/api/restaurant/image/${restaurant.id}`}
+        alt={`${restaurant.name} image`}
+        width="900px"
+      />
+      <Row>
+      <h3>{restaurant?.name}</h3>
+      </Row>
+      <Row>
+      <Col>
+      <h5>
+        Located at:
+        {' '}
+        {locationName}
+      </h5>
+      <h5>
+        Website:
+        {' '}
+        <a href={restaurant.website || '#'} target="_blank" rel="noopener noreferrer">
+        {restaurant?.website}
+        </a>
+      </h5>
+      <h5>
+        Phone:
+        {' '}
+        {restaurant?.phone}
+      </h5>
+      <h5>
+        Hours:
+        {' '}
+        {restaurant?.hours || 'Hours Not Available'}
+      </h5>
+      <a href={restaurant.menuLink || 'Menu Not Available'} target="_blank" rel="noopener noreferrer">
+        <h5>Menu</h5>
+      </a>
+      <a href={restaurant.onlineOrderLink || '#'} target="_blank" rel="noopener noreferrer">
+        <h5>Order Online</h5>
+      </a>
+      </Col>
+      <Col>
+      <h5>
+        Description:
+        {' '}
+        {restaurant.description || 'Description Not Available'}
+        </h5>
+      </Col>
+      </Row>
     </Container>
   );
 };
