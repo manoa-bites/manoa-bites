2b8f7fadfdb293a6e5fe20eaea51d9b50fdfe46f
pelitaf <pelita.felicitas@gmail.com>
2024-12-09 14:54:17 -1000

    updated search bar with working functions
    want better way to sort by location ID

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import { prisma } from '@/lib/prisma';
-import RestaurantCard from '@/components/RestaurantCard';
-import SearchBar from '@/components/SearchBar';
 import SearchBar2 from '@/components/SearchBar2';
+import SearchBar from '@/components/SearchBar';
 
 /** Renders a list of restuarants for the directory page. */
 const ListPage = async () => {
@@ -18,27 +17,15 @@ const ListPage = async () => {
   // console.log(stuff);
   return (
     <main>
-      <Container id="list" fluid className="py-3">
-        <Container>
-          <Row>
-            <Col>
-              <h1 className="text-center">Restaurants at Manoa</h1>
-              <Col md={8} className="mx-auto">
-                <SearchBar initialResults={[]} />
-              </Col>
-              <Row>
-                <SearchBar2 />
-              </Row>
-              <Row xs={1} md={2} lg={3} className="g-4">
-                {restaurants.map((restaurant) => (
-                  <Col key={restaurant.name}>
-                    <RestaurantCard restaurant={restaurant} />
-                  </Col>
-                ))}
-              </Row>
-            </Col>
-          </Row>
-        </Container>
+      <Container id="list" className="py-3">
+        <Row>
+          <Col>
+            <h1 className="text-center">Restaurants at Manoa</h1>
+          </Col>
+        </Row>
+        <Row>
+          <SearchBar2 initialRestaurants={restaurants} />
+        </Row>
       </Container>
     </main>
   );
